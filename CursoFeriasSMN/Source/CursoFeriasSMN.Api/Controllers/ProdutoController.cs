9251849268b4972a243c09dda2e776a059836602
Josiel <josielalv@hotmail.com>
2018-01-29 21:32:28 -0200

    Adicionando primeiro controller da api

---
+++ CursoFeriasSMN/Source/CursoFeriasSMN.Api/Controllers/ProdutoController.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CursoFeriasSMN.Repository.Repositories;
+
+namespace CursoFeriasSMN.Api.Controllers
+{
+    [RoutePrefix("api/produto")]
+    public class ProdutoController : ApiController
+    {
+        private readonly ProdutoRepository _produtoRepository = new ProdutoRepository();
+
+        public IHttpActionResult GetProdutos()
+        {
+            return Ok(_produtoRepository.GetProdutos());
+        }
+    }
+}
