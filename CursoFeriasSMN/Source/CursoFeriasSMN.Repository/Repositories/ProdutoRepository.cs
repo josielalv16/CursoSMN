5de2585f3c7714e0052ac2404b9b1e4dda92c9fc
Josiel <josielalv@hotmail.com>
2018-01-29 20:50:53 -0200

    Finalizando a camada de repositorio

---
+++ CursoFeriasSMN/Source/CursoFeriasSMN.Repository/Repositories/ProdutoRepository.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using CursoFeriasSMN.Domain.Entidades;
+using CursoFeriasSMN.Repository.DataBase;
+
+namespace CursoFeriasSMN.Repository.Repositories
+{
+    public class ProdutoRepository : Execucao
+    {
+        private static readonly Conexao Conexao = new Conexao();
+
+        public ProdutoRepository() : base(Conexao)
+        {
+        }
+
+        public IEnumerable<Produto> GetProdutos()
+        {
+            ExecuteProcedure("[dbo].[SP_SelProdutos]");
+
+            var produtos = new List<Produto>();
+
+            using (var reader = ExecuReader())
+            {
+                while (reader.Read())
+                    produtos.Add(new Produto
+                    {
+                        CodigoProduto = reader.ReadAsInt("CodigoProduto"),
+                        Nome = reader.ReadAsString("Nome"),
+                        Preco = reader.ReadAsDecimal("Preco"),
+                        Estoque = reader.ReadAsShort("Estoque")
+                    });
+            }
+
+            return produtos;
+        }
+
+        public enum Procedures
+        {
+            SP_SelProdutos,
+            SP_SelDadosProduto,
+            SP_InsProduto,
+            SP_UpdProduto,
+            SP_DelProduto,
+            TRG_HistoricoProduto
+        }
+    }
+}
